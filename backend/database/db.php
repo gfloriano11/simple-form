dddc059cf69333d89164cf9872f3d08efb53f105
gfloriano11 <gflorianodev@gmail.com>
2024-10-13 21:52:46 -0300

    feat: i did the read from only one user 'user-problem. not ended'

--- backend/database/db.php
+++ backend/database/db.php
@@ -1,10 +1,11 @@
 <?php
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'formulario';
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    define('servername', 'localhost');
+    define('username', 'root');
+    define('password', '');
+    define('dbname', 'formulario');
+
+    $conn = new mysqli(servername, username, password, dbname);
 
     if ($conn->connect_error){
         die('Conexão falhou: ' . $conn->connect_error);
